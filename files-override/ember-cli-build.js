a329214eeb9541a8d1447518599de63cf0b1c376
Chris Manson <chris@manson.ie>
2024-06-21 15:26:32 +0100

    make sure to initialise warp-drive config

--- files-override/ember-cli-build.js
+++ files-override/ember-cli-build.js
@@ -3,8 +3,13 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const { maybeEmbroider } = require('@embroider/test-setup');
 
-module.exports = function (defaults) {
+module.exports = async function (defaults) {
+  const { setConfig } = await import('@warp-drive/build-config');
   let app = new EmberApp(defaults, {});
+
+  setConfig(app, __dirname, {
+    // WarpDrive/EmberData settings go here (if any)
+  });
 
   return maybeEmbroider(app, {
     skipBabel: [
