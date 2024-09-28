b056098f64f931d5199b7ba98d8deb3b4285707d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-09-28 15:16:00 -0400

    Inline the environment.js for now

--- files/js/app/config/environment.js
+++ files/js/app/config/environment.js
@@ -1,3 +1,22 @@
-import loadConfigFromMeta from '@embroider/config-meta-loader';
+// import loadConfigFromMeta from '@embroider/config-meta-loader';
+//
+// export default loadConfigFromMeta('<%= name %>');
 
-export default loadConfigFromMeta('<%= name %>');
+export default {
+  modulePrefix: '<%= name %>',
+  environment: 'development',
+  rootURL: '/',
+  locationType: 'history',
+  EmberENV: {
+    EXTEND_PROTOTYPES: false,
+    FEATURES: {
+      // Here you can enable experimental features on an ember canary build
+      // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+    },
+  },
+
+  APP: {
+    // Here you can pass flags/options to your application instance
+    // when it is created
+  },
+};
