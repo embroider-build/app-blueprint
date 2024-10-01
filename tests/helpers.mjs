5de0c116de4fcd461cab9df7b1c9e063fecb9d48
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-01 08:54:03 -0400

    Extract an output:fixture command for manual testing

--- tests/helpers.mjs
+++ tests/helpers.mjs
@@ -1,12 +1,12 @@
 import assert from 'node:assert';
-import { join } from 'node:path';
 
 import { beforeAll, afterAll } from 'vitest';
 import { sync as resolveBinSync } from 'resolve-bin';
 import { execa } from 'execa';
 import tmp from 'tmp-promise';
 
-import copyWithTemplate from '../lib/copy-with-template';
+import copyWithTemplate from '../lib/copy-with-template.js';
+import { newApp } from '../lib/new-app.mjs';
 
 function findEmber() {
   return resolveBinSync('ember-cli', { executable: 'ember' });
@@ -14,7 +14,6 @@ function findEmber() {
 
 export const emberCli = findEmber();
 
-const blueprintPath = join(__dirname, '..');
 const appName = 'fancy-app-in-test';
 
 export function newProjectWithFixtures({
@@ -22,56 +21,42 @@ export function newProjectWithFixtures({
   fixturePath,
   name = appName,
 } = {}) {
-  let tmpDir;
+  let info;
 
   assert(fixturePath, `a fixturePath is required`);
 
   beforeAll(async () => {
-    tmpDir = await tmp.dir({ unsafeCleanup: true });
-
-    let emberCliArgs = [
-      'new',
-      name,
-      '-b',
-      blueprintPath,
-      '--pnpm',
-      '--skip-git',
-      ...flags,
-    ];
-
-    await execa(emberCli, emberCliArgs, {
-      cwd: tmpDir.path,
-      preferLocal: true,
-    });
+    info = await newApp({ name, flags: ['--skip-git', ...flags] });
 
     // apply the fixture on top of the generated app
-    copyWithTemplate(join(__dirname, 'fixture'), join(tmpDir.path, name), {
+    copyWithTemplate(fixturePath, info.dir, {
       name,
     });
 
     // Sync the lints for the fixtures with the project's config
     await execa(`pnpm`, ['lint:fix'], {
-      cwd: join(tmpDir.path, name),
+      cwd: info.dir,
     });
   });
 
   afterAll(async () => {
     try {
-      await tmpDir.cleanup();
+      await tmp.cleanup();
     } catch {
       // if it fails to cleaup we don't want to break CI
     }
   });
 
   return {
-    tmpDir: () => tmpDir.path,
+    tmp: () => info.tmpDir,
     appName: () => name,
-    dir: () => join(tmpDir.path, name),
-    $: (...args) => execa({ cwd: join(tmpDir.path, name) })(...args),
-    execa: (program, args, options = {}) =>
-      execa(program, args, {
-        cwd: join(tmpDir.path, name),
+    dir: () => info.dir,
+    $: (...args) => execa({ cwd: info.dir })(...args),
+    execa: (program, args, options = {}) => {
+      return execa(program, args, {
+        cwd: info.dir,
         ...options,
-      }),
+      });
+    },
   };
 }
