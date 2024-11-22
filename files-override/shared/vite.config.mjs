8ee791c7025f29f8b59f7678478c7809ab2dda9e
Liam Potter <liam@liampotter.co.uk>
2024-11-22 20:06:40 +0000

    simplify vite config

--- files-override/shared/vite.config.mjs
+++ files-override/shared/vite.config.mjs
@@ -1,64 +1,6 @@
 import { defineConfig } from 'vite';
-import {
-  resolver,
-  hbs,
-  scripts,
-  templateTag,
-  optimizeDeps,
-  compatPrebuild,
-  assets,
-  contentFor,
-} from '@embroider/vite';
-import { babel } from '@rollup/plugin-babel';
+import ember from '@embroider/vite';
 
-const extensions = [
-  '.mjs',
-  '.gjs',
-  '.js',
-  '.mts',
-  '.gts',
-  '.ts',
-  '.hbs',
-  '.json',
-];
-
-export default defineConfig(({ mode }) => {
-  return {
-    resolve: {
-      extensions,
-    },
-    plugins: [
-      hbs(),
-      templateTag(),
-      scripts(),
-      resolver(),
-      compatPrebuild(),
-      assets(),
-      contentFor(),
-
-      babel({
-        babelHelpers: 'runtime',
-        extensions,
-      }),
-    ],
-    optimizeDeps: optimizeDeps(),
-    server: {
-      port: 4200,
-    },
-    build: {
-      outDir: 'dist',
-      rollupOptions: {
-        input: {
-          main: 'index.html',
-          ...(shouldBuildTests(mode)
-            ? { tests: 'tests/index.html' }
-            : undefined),
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [ember()],
 });
-
-function shouldBuildTests(mode) {
-  return mode !== 'production' || process.env.FORCE_BUILD_TESTS;
-}
