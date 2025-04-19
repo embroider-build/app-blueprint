19f48fcd3f61447b12c860d3f7f862071cf189f0
void_malex <void-mAlex@users.noreply.github.com>
2025-04-19 23:13:55 +0100

    use generated app name

--- files-override/js/app/router.js
+++ files-override/js/app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@embroider/router';
-import config from 'my-fancy-app/config/environment';
+import config from '<%= name %>/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
